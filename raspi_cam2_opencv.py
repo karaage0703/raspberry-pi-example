aa4d1958f4b0680c760d3dcf8ee184c8ce2b4ce9
karaage0703 <coldchicken@gmail.com>
2022-04-03 11:08:36 +0900

    Add picamera2 example

---
+++ raspi_cam2_opencv.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# raspi cam2 demo
+import cv2
+
+from picamera2.picamera2 import *
+
+camera = Picamera2()
+camera.start_preview()
+camera.configure(camera.preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+camera.start()
+
+count_max = 0
+
+if __name__ == '__main__':
+    count = 0
+
+    while True:
+        img = camera.capture_array()
+
+        key = cv2.waitKey(1)
+        if key == 27: # when ESC key is pressed break
+            break
+
+        count += 1
+        if count > count_max:
+            cv2.imshow('View', img)
+            count = 0
+
+    camera.close()
+    cv2.destroyAllWindows()
